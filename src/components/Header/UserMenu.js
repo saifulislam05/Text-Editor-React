3cb9d62fe18732e0e06dcd1b0e5d9ebcab096f5d
saifulislam05 <saiful.bijulia@gmail.com>
2024-01-13 22:35:01 +0530

    Added Header section

---
+++ src/components/Header/UserMenu.js
@@ -0,0 +1,19 @@
+// UserMenu.js
+import React from "react";
+
+const UserMenu = () => {
+  return (
+    <div className="dropdown dropdown-end">
+      <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0kgW8G1yaDLMld6xFyPp8APVw792Vc40fkdMrykBZg-qi-oH4RiOszkys-Assdi5leCs&usqp=CAU"
+          />
+        </div>
+      </label>
+    </div>
+  );
+};
+
+export default UserMenu;
